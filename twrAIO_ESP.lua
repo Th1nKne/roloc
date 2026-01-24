04d0e5c7b41ccad04a366f14125f58bc4238021b
Th1nKne <thanhtran527589@gmail.com>
2026-01-24 11:20:18 +0700

    Add files via upload

---
+++ twrAIO_ESP.lua
@@ -0,0 +1,5 @@
+-- simple espbox zombie and item, prefix: L
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Th1nKne/roloc/refs/heads/main/twrESPs.lua"))()
+
+-- player espbox no prefix
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Th1nKne/roloc/refs/heads/main/espbox.lua"))()
